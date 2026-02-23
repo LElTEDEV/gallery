fb7e02a6fd2434890fd0f18206694366c193ff58
LElTEDEV <zinenizi@gmail.com>
2026-02-23 10:46:41 -0300

    feat: adicionado o component de mainContent

--- src/pages/layout-main.tsx
+++ src/pages/layout-main.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router";
+
 import { MainHeader } from "../components/main-header";
+import { MainContent } from "../components/main-content";
 
 export function LayoutMain() {
   return (
     <>
       <MainHeader />
-      <Outlet />;
+
+      <MainContent>
+        <Outlet />;
+      </MainContent>
     </>
   );
 }
