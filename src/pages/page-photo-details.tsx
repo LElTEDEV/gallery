9cd508847d78bf3d0e41e8e885e2a32dbc4010d0
LElTEDEV <zinenizi@gmail.com>
2026-02-23 13:37:47 -0300

    feat: adicionado a navegacao das fotos e feito mock de uma foto

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,13 +1,35 @@
 import { useParams } from "react-router";
 
 import Text from "../components/text";
+import Skeleton from "../components/skeleton";
+import Container from "../components/container";
+import { PhotosNavigator } from "../contexts/photos/components/photos-navigator";
 
 export function PagePhotoDetails() {
   const { id } = useParams() as { id: string };
+  const isLoadingPhoto = false;
+  const photo = {
+    id: "123",
+    title: "Olá Mundo",
+    imageId: "portrait-tower.png",
+    albums: [
+      { id: "123", title: "Album 1" },
+      { id: "321", title: "Album 2" },
+      { id: "123123", title: "Album 3" },
+    ],
+  };
 
   return (
-    <div>
-      <Text variant="heading-medium">Página detalhe da foto: {id}</Text>
-    </div>
+    <Container>
+      <header className="flex items-center justify-between gap-8 mb-8">
+        {!isLoadingPhoto ? (
+          <Text variant="heading-large">{photo.title}</Text>
+        ) : (
+          <Skeleton className="w-48 h-8" />
+        )}
+
+        <PhotosNavigator loading={isLoadingPhoto} />
+      </header>
+    </Container>
   );
 }
